38a13b71590c5cd997ec6e194cf070d91e89a215
Artem Maysky <ijezz@List.ru>
2026-04-10 11:39:02 +0300

    debug3

--- dist/bot.js
+++ dist/bot.js
@@ -1,15 +1,25 @@
-import { Telegraf, Markup } from 'telegraf';
+import { Telegraf, Markup, session } from 'telegraf';
 import dotenv from 'dotenv';
 dotenv.config();
-const BOT_TOKEN = process.env.BOT_TOKEN ?? '';
+const BOT_TOKEN = process.env.BOT_TOKEN;
+if (!BOT_TOKEN) {
+    throw new Error('BOT_TOKEN is missing in env');
+}
 const bot = new Telegraf(BOT_TOKEN);
+console.log('TOKEN:', process.env.BOT_TOKEN?.slice(0, 10));
 bot.start(ctx => {
-    ctx.reply('Выбери кнопку', Markup.inlineKeyboard([
-        [Markup.button.callback('Жми', 'Comand')]
+    ctx.reply(`
+        Привет, ${ctx.from.first_name}!
+        Для авторизации нажми на кнопку:
+    `, Markup.inlineKeyboard([
+        Markup.button.callback('🚪 Авторизироваться', 'auth')
     ]));
 });
-bot.action('comand', (ctx) => {
-    ctx.answerCbQuery('Нажато!');
-    ctx.reply('Ты нажал кнопку');
+bot.catch((err, ctx) => {
+    console.error('Ошибка:', err);
+    ctx.reply('Что-то пошло не так 😢');
 });
+bot.launch();
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 //# sourceMappingURL=bot.js.map

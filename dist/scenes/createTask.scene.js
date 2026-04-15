c3b1dbba69cced716bee9d9fcabcfa71e36234c4
Artem Maysky <ijezz@List.ru>
2026-04-15 10:01:26 +0300

    fix

--- dist/scenes/createTask.scene.js
+++ dist/scenes/createTask.scene.js
@@ -1,4 +1,4 @@
-import { Markup, Scenes } from 'telegraf';
+import { Scenes } from 'telegraf';
 function getMessageText(ctx) {
     if (!ctx.message || !('text' in ctx.message)) {
         ctx.reply('Отправь текст');
@@ -7,21 +7,22 @@ function getMessageText(ctx) {
     return ctx.message.text;
 }
 export const createTaskScene = new Scenes.WizardScene('createTaskScene', async (ctx) => {
-    await ctx.reply('Введи название задачи: ');
+    await ctx.reply('📃 Введи название задачи: ');
     return ctx.wizard.next();
 }, async (ctx) => {
     ctx.wizard.state.title = getMessageText(ctx);
-    ctx.reply('Введи описание задачи: ');
+    ctx.reply('📃 Введи описание задачи: ');
     return ctx.wizard.next();
 }, async (ctx) => {
     ctx.wizard.state.description = getMessageText(ctx);
-    ctx.reply('Введите дату дедлайна в формате дд.мм.гггг: ');
+    ctx.reply('📆 Введите дату дедлайна в формате дд.мм.гггг: ');
     return ctx.wizard.next();
 }, async (ctx) => {
     let dateString = getMessageText(ctx);
-    if (!dateString || !isNaN(Date.parse(dateString))) {
-        return ctx.reply('Дата неправильного формата');
+    if (!dateString || isNaN(Date.parse(dateString))) {
+        return ctx.reply('❌ Дата неправильного формата');
     }
     ctx.wizard.state.deadline = new Date(dateString);
+    return ctx.scene.enter('menuScene');
 });
 //# sourceMappingURL=createTask.scene.js.map
--- src/scenes/createTask.scene.ts
+++ src/scenes/createTask.scene.ts
@@ -1,5 +1,5 @@
-import { Markup, Scenes } from 'telegraf'
-import type { BotContext, TaskState } from '../context.js';
+import { Scenes } from 'telegraf'
+import type { BotContext } from '../context.js';
 
 function getMessageText(ctx: BotContext): string {
     if (!ctx.message || !('text' in ctx.message)) {
@@ -14,31 +14,31 @@ export const createTaskScene = new Scenes.WizardScene<BotContext>(
     'createTaskScene',
     
     async ctx => {
-        await ctx.reply('Введи название задачи: ');
+        await ctx.reply('📃 Введи название задачи: ');
         return ctx.wizard.next();
     },
 
     async ctx => {
         ctx.wizard.state.title = getMessageText(ctx);
-        ctx.reply('Введи описание задачи: ');
+        ctx.reply('📃 Введи описание задачи: ');
         return ctx.wizard.next();
     },
 
     async ctx => {
         ctx.wizard.state.description = getMessageText(ctx);
-        ctx.reply('Введите дату дедлайна в формате дд.мм.гггг: ')
+        ctx.reply('📆 Введите дату дедлайна в формате дд.мм.гггг: ')
         return ctx.wizard.next();
     },
 
     async ctx=> {
         let dateString: string = getMessageText(ctx)
 
-        if (!dateString || !isNaN(Date.parse(dateString))) {
-            return ctx.reply('Дата неправильного формата')
+        if (!dateString || isNaN(Date.parse(dateString))) {
+            return ctx.reply('❌ Дата неправильного формата')
         }
 
         ctx.wizard.state.deadline = new Date(dateString);
-        
+        return ctx.scene.enter('menuScene');
     }
 
 );

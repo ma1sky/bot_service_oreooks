0ee0fc0f874c005fbb52d8c7b821e28fd47b8a6a
Artem Maysky <ijezz@List.ru>
2026-04-24 09:48:37 +0300

    fix

--- src/scenes/menu.scene.ts
+++ src/scenes/menu.scene.ts
@@ -4,7 +4,7 @@ import type { BotContext } from '../config/types.js';
 export const menuScene = new Scenes.BaseScene<BotContext>('menuScene');
 
 menuScene.enter(async ctx => {
-    await ctx.editMessageText('📋 Меню:', 
+    await ctx.reply('📋 Меню:', 
         Markup.inlineKeyboard([
             [Markup.button.callback('➕ Создать задачу','createTask')],
             [Markup.button.callback('📆 Показать расписание', 'openSchedule')],

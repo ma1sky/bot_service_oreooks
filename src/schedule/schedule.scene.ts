a8f808489567a47babd8dddc207345adb93ff5a5
Artem Maysky <ijezz@List.ru>
2026-05-08 00:20:49 +0300

    auth 2

--- src/schedule/schedule.scene.ts
+++ src/schedule/schedule.scene.ts
@@ -1,34 +1,34 @@
-import { Markup, Scenes } from "telegraf";
-import type { BotContext } from "../config/types";
-import ScheduleService from "./schedule.service";
+// import { Markup, Scenes } from "telegraf";
+// import type { BotContext } from "../config/types";
+// import ScheduleService from "./schedule.service";
 
 
-export const scheduleScene = new Scenes.BaseScene<BotContext>('scheduleScene');
+// export const scheduleScene = new Scenes.BaseScene<BotContext>('scheduleScene');
 
-scheduleScene.enter(async (ctx) => {
+// scheduleScene.enter(async (ctx) => {
 
-	ctx.scene.session
+// 	ctx.scene.session
 
-	await ctx.editMessageText('' ,{
-		parse_mode: 'HTML',
-		...Markup.inlineKeyboard([
-			[
-				Markup.button.callback('◀️', 'openYesterday'),
-				Markup.button.callback('📋 Меню', 'openMenu'),
-				Markup.button.callback('▶️', 'openTomorrow'),
-			],
-		]),
-	});
-});
+// 	await ctx.editMessageText('' ,{
+// 		parse_mode: 'HTML',
+// 		...Markup.inlineKeyboard([
+// 			[
+// 				Markup.button.callback('◀️', 'openYesterday'),
+// 				Markup.button.callback('📋 Меню', 'openMenu'),
+// 				Markup.button.callback('▶️', 'openTomorrow'),
+// 			],
+// 		]),
+// 	});
+// });
 
-scheduleScene.action('openMenu', async ctx => {
-    await ctx.scene.enter('menuScene');
-})
+// scheduleScene.action('openMenu', async ctx => {
+//     await ctx.scene.enter('menuScene');
+// })
 
-scheduleScene.action('openYesterday', ctx => {
+// scheduleScene.action('openYesterday', ctx => {
 	
-})
+// })
 
-scheduleScene.action('openTomorrow', ctx => {
+// scheduleScene.action('openTomorrow', ctx => {
     
-})
+// })
